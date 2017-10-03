f195588174b3a5f07afe7393dd31420bdd1b1f7d
jack6785123 <32056027+jack6785123@users.noreply.github.com>
2017-10-03 11:44:39 +0800

    34

---
+++ src/hw5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class hw5 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int val1 = scn.nextInt();
+        int val2 = scn.nextInt();
+        System.out.println(val1/2.54d);
+        System.out.println(val2/0.454d);
+    }
+
+}
---
+++ src/hw6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class hw6 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        char ch1  = scn.next().charAt(0);
+        System.out.println(str.replace(ch1,'\n'));
+
+    }
+}
